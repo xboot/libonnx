297d32a4fcee17cc6bbe7a6473ddd5afa2259225
JianJunJiang <8192542@qq.com>
2020-10-16 00:50:08 +0800

    [helper]add helper function

---
+++ src/helper.h
@@ -0,0 +1,49 @@
+#ifndef __HELPER_H__
+#define __HELPER_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdint.h>
+
+static inline uint16_t float32_to_float16(float v)
+{
+	uint32_t * p = (uint32_t *)&v;
+	uint32_t t = *p;
+	uint16_t value;
+
+	value = ((t & 0x7fffffff) >> 13) - (0x38000000 >> 13);
+	value |= ((t & 0x80000000) >> 16);
+	return value;
+}
+
+static inline float float16_to_float32(uint16_t v)
+{
+	uint32_t t = v;
+	float * p = (float *)&t;
+
+	t = ((t & 0x7fff) << 13) + 0x38000000;
+	t |= ((v & 0x8000) << 16);
+	return *p;
+}
+
+static inline uint16_t float32_to_bfloat16(float v)
+{
+	union { uint32_t u; float f; } t;
+	t.f = v;
+	return t.u >> 16;
+}
+
+static inline float bfloat16_to_float32(uint16_t v)
+{
+	union { uint32_t u; float f; } t;
+	t.u = v << 16;
+	return t.f;
+}
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __HELPER_H__ */
