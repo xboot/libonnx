fce3227244f5a82b1790ffc2a45ffa16d9e1f22b
JianjunJiang <8192542@qq.com>
2020-10-15 19:16:20 +0800

    [tests]update node test

--- tests/node/main.c
+++ tests/node/main.c
@@ -7,6 +7,8 @@
 #include <libgen.h>
 #include <onnx.h>
 
+#define FLOAT_PRECISION		(0.001f)
+
 struct testcase_t {
 	const char * name;
 	const char * model_file;
@@ -31,69 +33,100 @@ static struct testcase_t testcases[] = {
 	},
 };
 
-static Onnx__TensorProto * pb_tensor_alloc(const char * filename)
+static int onnx_tensor_equal(Onnx__TensorProto * a, Onnx__TensorProto * b)
 {
-	Onnx__TensorProto * t = NULL;
-	FILE * fp;
-	void * buf;
-	size_t l, len;
+	int i;
 
-	fp = fopen(filename, "rb");
-	if(fp)
-	{
-		fseek(fp, 0L, SEEK_END);
-		l = ftell(fp);
-		fseek(fp, 0L, SEEK_SET);
-		if(l > 0)
-		{
-			buf = malloc(l);
-			if(buf)
-			{
-				for(len = 0; len < l; len += fread(buf + len, 1, l - len, fp));
-				t = onnx__tensor_proto__unpack(NULL, len, buf);
-				free(buf);
-			}
-		}
-	}
-	fclose(fp);
-	return t;
-}
-
-static void pb_tensor_free(Onnx__TensorProto * t)
-{
-	if(t)
-		onnx__tensor_proto__free_unpacked(t, NULL);
-}
-
-static int testcase_run(struct resolver_t * r, struct testcase_t * tc)
-{
-	struct onnx_context_t * ctx;
-	Onnx__TensorProto * t;
-
-	if(!tc)
+	if(!a || !b)
 		return 0;
-
-	ctx = onnx_context_alloc_from_file(tc->model_file, r);
-	if(ctx)
+	if(a->data_type != b->data_type)
+		return 0;
+	if(a->n_dims != b->n_dims)
+		return 0;
+	if(memcmp(a->dims, b->dims, sizeof(int64_t) * a->n_dims) != 0)
+		return 0;
+	switch(a->data_type)
 	{
-		t = pb_tensor_alloc(tc->input_files[0]);
-		pb_tensor_free(t);
-
-		onnx_dump_model(ctx);
+	case ONNX__TENSOR_PROTO__DATA_TYPE__FLOAT:
+		if(a->float_data != b->float_data)
+			return 0;
+		for(i = 0; i < a->n_float_data; i++)
+		{
+			if(fabs(a->float_data[i] - b->float_data[i]) > FLOAT_PRECISION)
+				return 0;
+		}
+		break;
+	case ONNX__TENSOR_PROTO__DATA_TYPE__UINT8:
+		break;
+	case ONNX__TENSOR_PROTO__DATA_TYPE__INT8:
+		break;
+	case ONNX__TENSOR_PROTO__DATA_TYPE__UINT16:
+		break;
+	case ONNX__TENSOR_PROTO__DATA_TYPE__INT16:
+		break;
+	case ONNX__TENSOR_PROTO__DATA_TYPE__INT32:
+		break;
+	case ONNX__TENSOR_PROTO__DATA_TYPE__INT64:
+		break;
+	case ONNX__TENSOR_PROTO__DATA_TYPE__STRING:
+		break;
+	case ONNX__TENSOR_PROTO__DATA_TYPE__BOOL:
+		break;
+	case ONNX__TENSOR_PROTO__DATA_TYPE__FLOAT16:
+		break;
+	case ONNX__TENSOR_PROTO__DATA_TYPE__DOUBLE:
+		break;
+	case ONNX__TENSOR_PROTO__DATA_TYPE__UINT32:
+		break;
+	case ONNX__TENSOR_PROTO__DATA_TYPE__UINT64:
+		break;
+	case ONNX__TENSOR_PROTO__DATA_TYPE__COMPLEX64:
+		break;
+	case ONNX__TENSOR_PROTO__DATA_TYPE__COMPLEX128:
+		break;
+	case ONNX__TENSOR_PROTO__DATA_TYPE__BFLOAT16:
+		break;
+	default:
+		break;
 	}
 	return 1;
 }
 
-static void testcase_print(const char * name, int result)
+static void testcase_run(struct resolver_t * r, struct testcase_t * tc)
 {
-	int len = printf("\033[43;37m[%s]\033[0m", name);
-	printf("%*s\r\n", 80 + 12 - 6 - len, result ? "\033[42;37m[OKAY]\033[0m" : "\033[41;37m[FAIL]\033[0m");
+	struct onnx_context_t * ctx;
+	Onnx__TensorProto * t, * o;
+	int fail = 0;
+	int i;
+
+	ctx = onnx_context_alloc_from_file(tc->model_file, r);
+	if(ctx)
+	{
+		for(i = 0; i < tc->ninput; i++)
+		{
+			t = onnx_search_tensor(ctx, tc->input_names[i]);
+			o = onnx_tensor_alloc_from_file(tc->input_files[i]);
+			onnx_tensor_copy(t, o);
+			onnx_tensor_free(o);
+		}
+		onnx_run(ctx);
+		for(i = 0; i < tc->noutput; i++)
+		{
+			t = onnx_search_tensor(ctx, tc->output_names[i]);
+			o = onnx_tensor_alloc_from_file(tc->output_files[i]);
+			if(!onnx_tensor_equal(t, o))
+				fail |= 1;
+			onnx_tensor_free(o);
+		}
+		onnx_context_free(ctx);
+	}
+	i = printf("\033[43;37m[%s]\033[0m", tc->name);
+	printf("%*s\r\n", 80 + 12 - 6 - i, fail ? "\033[41;37m[FAIL]\033[0m" : "\033[42;37m[OKAY]\033[0m");
 }
 
 int main(int argc, char * argv[])
 {
 	struct resolver_t * r = NULL;
-	struct testcase_t * tc;
 	char path[PATH_MAX];
 	int i;
 
@@ -101,9 +134,7 @@ int main(int argc, char * argv[])
 		printf("ERROR: Can't change working directory.(%s)\r\n", getcwd(path, sizeof(path)));
 
 	for(i = 0; i < (sizeof(testcases) / sizeof((testcases)[0])); i++)
-	{
-		tc = &testcases[i];
-		testcase_print(tc->name, testcase_run(r, tc));
-	}
+		testcase_run(r, &testcases[i]);
+
 	return 0;
 }
