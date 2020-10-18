9324eeac8953d373534727ab6582424cef60f22c
JianJunJiang <8192542@qq.com>
2020-10-18 21:08:43 +0800

    [operator]add Elu operator

--- src/default/Elu.c
+++ src/default/Elu.c
@@ -1,5 +1,98 @@
 #include <onnx.h>
 
+struct operator_pdata_t {
+	float alpha;
+};
+
+static void Elu_init(struct onnx_node_t * n)
+{
+	struct operator_pdata_t * pdat;
+	struct onnx_tensor_t * t = n->inputs[0];
+	int i;
+
+	for(i = 0; i < n->noutput; i++)
+	{
+		if(n->outputs[i]->type == ONNX_TENSOR_TYPE_UNDEFINED)
+			onnx_tensor_reinit(n->outputs[i], t->type, t->dims, t->ndim);
+	}
+
+	pdat = malloc(sizeof(struct operator_pdata_t));
+	if(pdat)
+		pdat->alpha = onnx_attribute_read_float(n, "alpha", 1.0);
+	n->priv = pdat;
+}
+
+static void Elu_exit(struct onnx_node_t * n)
+{
+	struct operator_pdata_t * pdat = (struct operator_pdata_t *)n->priv;
+
+	if(pdat)
+		free(pdat);
+}
+
+static void Elu_float16(struct onnx_node_t * n)
+{
+	struct operator_pdata_t * pdat = (struct operator_pdata_t *)n->priv;
+	struct onnx_tensor_t * x = n->inputs[0];
+	struct onnx_tensor_t * y = n->outputs[0];
+	uint16_t * px = (uint16_t *)x->datas;
+	uint16_t * py = (uint16_t *)y->datas;
+	float v;
+	int i, l;
+
+	for(i = 0, l = y->ndata; i < l; i++)
+	{
+		v = float16_to_float32(px[i]);
+		py[i] = float32_to_float16((px[i] < 0) ? (expf(v) - 1) * pdat->alpha : v);
+	}
+}
+
+static void Elu_float32(struct onnx_node_t * n)
+{
+	struct operator_pdata_t * pdat = (struct operator_pdata_t *)n->priv;
+	struct onnx_tensor_t * x = n->inputs[0];
+	struct onnx_tensor_t * y = n->outputs[0];
+	float * px = (float *)x->datas;
+	float * py = (float *)y->datas;
+	int i, l;
+
+	for(i = 0, l = y->ndata; i < l; i++)
+		py[i] = (px[i] < 0) ? (expf(px[i]) - 1) * pdat->alpha : px[i];
+}
+
+static void Elu_float64(struct onnx_node_t * n)
+{
+	struct operator_pdata_t * pdat = (struct operator_pdata_t *)n->priv;
+	struct onnx_tensor_t * x = n->inputs[0];
+	struct onnx_tensor_t * y = n->outputs[0];
+	double * px = (double *)x->datas;
+	double * py = (double *)y->datas;
+	int i, l;
+
+	for(i = 0, l = y->ndata; i < l; i++)
+		py[i] = (px[i] < 0) ? (exp(px[i]) - 1) * pdat->alpha : px[i];
+}
+
 void default_resolver_op_Elu(struct onnx_node_t * n)
 {
+	switch(n->inputs[0]->type)
+	{
+	case ONNX_TENSOR_TYPE_FLOAT16:
+		n->init = Elu_init;
+		n->exit = Elu_exit;
+		n->op = Elu_float16;
+		break;
+	case ONNX_TENSOR_TYPE_FLOAT32:
+		n->init = Elu_init;
+		n->exit = Elu_exit;
+		n->op = Elu_float32;
+		break;
+	case ONNX_TENSOR_TYPE_FLOAT64:
+		n->init = Elu_init;
+		n->exit = Elu_exit;
+		n->op = Elu_float64;
+		break;
+	default:
+		break;
+	}
 }
