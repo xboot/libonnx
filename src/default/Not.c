445b06bc55dfab37c7ea35f718b15f7771aa4762
JianJunJiang <8192542@qq.com>
2020-10-18 22:45:46 +0800

    [operator]add Not operator

--- src/default/Not.c
+++ src/default/Not.c
@@ -1,5 +1,43 @@
 #include <onnx.h>
 
+static void Not_init(struct onnx_node_t * n)
+{
+	struct onnx_tensor_t * t = n->inputs[0];
+	int i;
+
+	for(i = 0; i < n->noutput; i++)
+	{
+		if(n->outputs[i]->type == ONNX_TENSOR_TYPE_UNDEFINED)
+			onnx_tensor_reinit(n->outputs[i], t->type, t->dims, t->ndim);
+	}
+}
+
+static void Not_exit(struct onnx_node_t * n)
+{
+}
+
+static void Not_bool(struct onnx_node_t * n)
+{
+	struct onnx_tensor_t * x = n->inputs[0];
+	struct onnx_tensor_t * y = n->outputs[0];
+	uint8_t * px = (uint8_t *)x->datas;
+	uint8_t * py = (uint8_t *)y->datas;
+	int i, l;
+
+	for(i = 0, l = y->ndata; i < l; i++)
+		py[i] = !px[i];
+}
+
 void default_resolver_op_Not(struct onnx_node_t * n)
 {
+	switch(n->inputs[0]->type)
+	{
+	case ONNX_TENSOR_TYPE_BOOL:
+		n->init = Not_init;
+		n->exit = Not_exit;
+		n->op = Not_bool;
+		break;
+	default:
+		break;
+	}
 }
