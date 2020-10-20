ba33e921657b591d43d70e0cfc4d2769493db7b4
JianjunJiang <8192542@qq.com>
2020-10-20 17:05:10 +0800

    [operator]add Celu operator

--- src/default/Celu.c
+++ src/default/Celu.c
@@ -1,5 +1,58 @@
 #include <onnx.h>
 
+struct operator_pdata_t {
+	float alpha;
+};
+
+static void Celu_init(struct onnx_node_t * n)
+{
+	struct operator_pdata_t * pdat;
+	struct onnx_tensor_t * t = n->inputs[0];
+	int i;
+
+	for(i = 0; i < n->noutput; i++)
+	{
+		if(n->outputs[i]->type == ONNX_TENSOR_TYPE_UNDEFINED)
+			onnx_tensor_reinit(n->outputs[i], t->type, t->dims, t->ndim);
+	}
+
+	pdat = malloc(sizeof(struct operator_pdata_t));
+	if(pdat)
+		pdat->alpha = onnx_attribute_read_float(n, "alpha", 1.0);
+	n->priv = pdat;
+}
+
+static void Celu_exit(struct onnx_node_t * n)
+{
+	struct operator_pdata_t * pdat = (struct operator_pdata_t *)n->priv;
+
+	if(pdat)
+		free(pdat);
+}
+
+static void Celu_float32(struct onnx_node_t * n)
+{
+	struct operator_pdata_t * pdat = (struct operator_pdata_t *)n->priv;
+	struct onnx_tensor_t * x = n->inputs[0];
+	struct onnx_tensor_t * y = n->outputs[0];
+	float * px = (float *)x->datas;
+	float * py = (float *)y->datas;
+	int i, l;
+
+	for(i = 0, l = y->ndata; i < l; i++)
+		py[i] = max((float)0.0, (float)px[i]) + min((float)0.0, (float)pdat->alpha * (expf(px[i] / pdat->alpha) - 1));
+}
+
 void default_resolver_op_Celu(struct onnx_node_t * n)
 {
+	switch(n->inputs[0]->type)
+	{
+	case ONNX_TENSOR_TYPE_FLOAT32:
+		n->init = Celu_init;
+		n->exit = Celu_exit;
+		n->op = Celu_float32;
+		break;
+	default:
+		break;
+	}
 }
