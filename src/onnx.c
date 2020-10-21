d8a3b950a93f9f587facba087d773cf168ab2723
JianjunJiang <8192542@qq.com>
2020-10-21 11:50:02 +0800

    [libonnx]rename resolver operator to rop and run operator one times at the end of init

--- src/onnx.c
+++ src/onnx.c
@@ -195,500 +195,500 @@ static struct resolver_t default_resolver = {
 
 static void resolver_solve_operator(struct resolver_t * r, struct onnx_node_t * n)
 {
-	void (*op)(struct onnx_node_t *);
+	void (*rop)(struct onnx_node_t *);
 
 	if(r && n)
 	{
 		switch(shash(n->proto->op_type))
 		{
 		case 0x0b87d47b: /* "Abs" */
-			op = r->op_Abs;
+			rop = r->op_Abs;
 			break;
 		case 0x7c82680b: /* "Acos" */
-			op = r->op_Acos;
+			rop = r->op_Acos;
 			break;
 		case 0x0ccf69d3: /* "Acosh" */
-			op = r->op_Acosh;
+			rop = r->op_Acosh;
 			break;
 		case 0x0b87d4ae: /* "Add" */
-			op = r->op_Add;
+			rop = r->op_Add;
 			break;
 		case 0x0b87d5f8: /* "And" */
-			op = r->op_And;
+			rop = r->op_And;
 			break;
 		case 0xa7c70ea5: /* "ArgMax" */
-			op = r->op_ArgMax;
+			rop = r->op_ArgMax;
 			break;
 		case 0xa7c70fa3: /* "ArgMin" */
-			op = r->op_ArgMin;
+			rop = r->op_ArgMin;
 			break;
 		case 0x7c82ab50: /* "Asin" */
-			op = r->op_Asin;
+			rop = r->op_Asin;
 			break;
 		case 0x0cd815b8: /* "Asinh" */
-			op = r->op_Asinh;
+			rop = r->op_Asinh;
 			break;
 		case 0x7c82ae89: /* "Atan" */
-			op = r->op_Atan;
+			rop = r->op_Atan;
 			break;
 		case 0x0cd88011: /* "Atanh" */
-			op = r->op_Atanh;
+			rop = r->op_Atanh;
 			break;
 		case 0xf1a1e23a: /* "AveragePool" */
-			op = r->op_AveragePool;
+			rop = r->op_AveragePool;
 			break;
 		case 0x2d3b46ee: /* "BatchNormalization" */
-			op = r->op_BatchNormalization;
+			rop = r->op_BatchNormalization;
 			break;
 		case 0x0bfe45a2: /* "BitShift" */
-			op = r->op_BitShift;
+			rop = r->op_BitShift;
 			break;
 		case 0x7c8378d0: /* "Cast" */
-			op = r->op_Cast;
+			rop = r->op_Cast;
 			break;
 		case 0x7c838882: /* "Ceil" */
-			op = r->op_Ceil;
+			rop = r->op_Ceil;
 			break;
 		case 0x7c83a64d: /* "Clip" */
-			op = r->op_Clip;
+			rop = r->op_Clip;
 			break;
 		case 0xb7db9db1: /* "Compress" */
-			op = r->op_Compress;
+			rop = r->op_Compress;
 			break;
 		case 0xac3f4a9d: /* "Concat" */
-			op = r->op_Concat;
+			rop = r->op_Concat;
 			break;
 		case 0x5053caca: /* "ConcatFromSequence" */
-			op = r->op_ConcatFromSequence;
+			rop = r->op_ConcatFromSequence;
 			break;
 		case 0xba6816ef: /* "Constant" */
-			op = r->op_Constant;
+			rop = r->op_Constant;
 			break;
 		case 0xe468a875: /* "ConstantOfShape" */
-			op = r->op_ConstantOfShape;
+			rop = r->op_ConstantOfShape;
 			break;
 		case 0x7c83b3bb: /* "Conv" */
-			op = r->op_Conv;
+			rop = r->op_Conv;
 			break;
 		case 0x8371dbe9: /* "ConvInteger" */
-			op = r->op_ConvInteger;
+			rop = r->op_ConvInteger;
 			break;
 		case 0x3903c4ba: /* "ConvTranspose" */
-			op = r->op_ConvTranspose;
+			rop = r->op_ConvTranspose;
 			break;
 		case 0x0b87deaa: /* "Cos" */
-			op = r->op_Cos;
+			rop = r->op_Cos;
 			break;
 		case 0x7c83b452: /* "Cosh" */
-			op = r->op_Cosh;
+			rop = r->op_Cosh;
 			break;
 		case 0xacab0fbf: /* "CumSum" */
-			op = r->op_CumSum;
+			rop = r->op_CumSum;
 			break;
 		case 0xc9c1d669: /* "DepthToSpace" */
-			op = r->op_DepthToSpace;
+			rop = r->op_DepthToSpace;
 			break;
 		case 0xf9cc985a: /* "DequantizeLinear" */
-			op = r->op_DequantizeLinear;
+			rop = r->op_DequantizeLinear;
 			break;
 		case 0x0b87e1a2: /* "Det" */
-			op = r->op_Det;
+			rop = r->op_Det;
 			break;
 		case 0x0b87e228: /* "Div" */
-			op = r->op_Div;
+			rop = r->op_Div;
 			break;
 		case 0x883bca72: /* "Dropout" */
-			op = r->op_Dropout;
+			rop = r->op_Dropout;
 			break;
 		case 0xb07d4f76: /* "Einsum" */
-			op = r->op_Einsum;
+			rop = r->op_Einsum;
 			break;
 		case 0x0b87e6cb: /* "Elu" */
-			op = r->op_Elu;
+			rop = r->op_Elu;
 			break;
 		case 0x0d1f905d: /* "Equal" */
-			op = r->op_Equal;
+			rop = r->op_Equal;
 			break;
 		case 0x0b87e782: /* "Erf" */
-			op = r->op_Erf;
+			rop = r->op_Erf;
 			break;
 		case 0x0b87e852: /* "Exp" */
-			op = r->op_Exp;
+			rop = r->op_Exp;
 			break;
 		case 0xb18d8a45: /* "Expand" */
-			op = r->op_Expand;
+			rop = r->op_Expand;
 			break;
 		case 0xe4c1560d: /* "EyeLike" */
-			op = r->op_EyeLike;
+			rop = r->op_EyeLike;
 			break;
 		case 0x13363dd3: /* "Flatten" */
-			op = r->op_Flatten;
+			rop = r->op_Flatten;
 			break;
 		case 0x0d2ed347: /* "Floor" */
-			op = r->op_Floor;
+			rop = r->op_Floor;
 			break;
 		case 0x0b87ebd3: /* "GRU" */
-			op = r->op_GRU;
+			rop = r->op_GRU;
 			break;
 		case 0xb499f620: /* "Gather" */
-			op = r->op_Gather;
+			rop = r->op_Gather;
 			break;
 		case 0x7c94d43d: /* "GatherElements" */
-			op = r->op_GatherElements;
+			rop = r->op_GatherElements;
 			break;
 		case 0x42f00872: /* "GatherND" */
-			op = r->op_GatherND;
+			rop = r->op_GatherND;
 			break;
 		case 0x7c85ba8b: /* "Gemm" */
-			op = r->op_Gemm;
+			rop = r->op_Gemm;
 			break;
 		case 0x9289c84b: /* "GlobalAveragePool" */
-			op = r->op_GlobalAveragePool;
+			rop = r->op_GlobalAveragePool;
 			break;
 		case 0x3f5a29ac: /* "GlobalLpPool" */
-			op = r->op_GlobalLpPool;
+			rop = r->op_GlobalLpPool;
 			break;
 		case 0x575f0fb6: /* "GlobalMaxPool" */
-			op = r->op_GlobalMaxPool;
+			rop = r->op_GlobalMaxPool;
 			break;
 		case 0x6e6d652f: /* "Greater" */
-			op = r->op_Greater;
+			rop = r->op_Greater;
 			break;
 		case 0x10341df0: /* "HardSigmoid" */
-			op = r->op_HardSigmoid;
+			rop = r->op_HardSigmoid;
 			break;
 		case 0x94acb4aa: /* "Hardmax" */
-			op = r->op_Hardmax;
+			rop = r->op_Hardmax;
 			break;
 		case 0xdfd9b28f: /* "Identity" */
-			op = r->op_Identity;
+			rop = r->op_Identity;
 			break;
 		case 0x00597414: /* "If" */
-			op = r->op_If;
+			rop = r->op_If;
 			break;
 		case 0xfb0902c1: /* "InstanceNormalization" */
-			op = r->op_InstanceNormalization;
+			rop = r->op_InstanceNormalization;
 			break;
 		case 0x0d68519e: /* "IsInf" */
-			op = r->op_IsInf;
+			rop = r->op_IsInf;
 			break;
 		case 0x0d68651e: /* "IsNaN" */
-			op = r->op_IsNaN;
+			rop = r->op_IsNaN;
 			break;
 		case 0x0b880111: /* "LRN" */
-			op = r->op_LRN;
+			rop = r->op_LRN;
 			break;
 		case 0x7c882885: /* "LSTM" */
-			op = r->op_LSTM;
+			rop = r->op_LSTM;
 			break;
 		case 0xea2c5c33: /* "LeakyRelu" */
-			op = r->op_LeakyRelu;
+			rop = r->op_LeakyRelu;
 			break;
 		case 0x7c88793c: /* "Less" */
-			op = r->op_Less;
+			rop = r->op_Less;
 			break;
 		case 0x0b8804e7: /* "Log" */
-			op = r->op_Log;
+			rop = r->op_Log;
 			break;
 		case 0x7c88a33f: /* "Loop" */
-			op = r->op_Loop;
+			rop = r->op_Loop;
 			break;
 		case 0x07f77ce8: /* "LpNormalization" */
-			op = r->op_LpNormalization;
+			rop = r->op_LpNormalization;
 			break;
 		case 0xc13f923b: /* "LpPool" */
-			op = r->op_LpPool;
+			rop = r->op_LpPool;
 			break;
 		case 0xc2987915: /* "MatMul" */
-			op = r->op_MatMul;
+			rop = r->op_MatMul;
 			break;
 		case 0x62fbd803: /* "MatMulInteger" */
-			op = r->op_MatMulInteger;
+			rop = r->op_MatMulInteger;
 			break;
 		case 0x0b88076b: /* "Max" */
-			op = r->op_Max;
+			rop = r->op_Max;
 			break;
 		case 0x15f18a25: /* "MaxPool" */
-			op = r->op_MaxPool;
+			rop = r->op_MaxPool;
 			break;
 		case 0x018c06cf: /* "MaxRoiPool" */
-			op = r->op_MaxRoiPool;
+			rop = r->op_MaxRoiPool;
 			break;
 		case 0x641501e8: /* "MaxUnpool" */
-			op = r->op_MaxUnpool;
+			rop = r->op_MaxUnpool;
 			break;
 		case 0x7c890346: /* "Mean" */
-			op = r->op_Mean;
+			rop = r->op_Mean;
 			break;
 		case 0x0b880869: /* "Min" */
-			op = r->op_Min;
+			rop = r->op_Min;
 			break;
 		case 0x0b880925: /* "Mod" */
-			op = r->op_Mod;
+			rop = r->op_Mod;
 			break;
 		case 0x0b8809f3: /* "Mul" */
-			op = r->op_Mul;
+			rop = r->op_Mul;
 			break;
 		case 0xaec55410: /* "Multinomial" */
-			op = r->op_Multinomial;
+			rop = r->op_Multinomial;
 			break;
 		case 0x0b880c1f: /* "Neg" */
-			op = r->op_Neg;
+			rop = r->op_Neg;
 			break;
 		case 0x254e25a1: /* "NonMaxSuppression" */
-			op = r->op_NonMaxSuppression;
+			rop = r->op_NonMaxSuppression;
 			break;
 		case 0x82e45c50: /* "NonZero" */
-			op = r->op_NonZero;
+			rop = r->op_NonZero;
 			break;
 		case 0x0b880d76: /* "Not" */
-			op = r->op_Not;
+			rop = r->op_Not;
 			break;
 		case 0xc825b932: /* "OneHot" */
-			op = r->op_OneHot;
+			rop = r->op_OneHot;
 			break;
 		case 0x005974e6: /* "Or" */
-			op = r->op_Or;
+			rop = r->op_Or;
 			break;
 		case 0x0dd55b8d: /* "PRelu" */
-			op = r->op_PRelu;
+			rop = r->op_PRelu;
 			break;
 		case 0x0b88141a: /* "Pad" */
-			op = r->op_Pad;
+			rop = r->op_Pad;
 			break;
 		case 0x0b8815fb: /* "Pow" */
-			op = r->op_Pow;
+			rop = r->op_Pow;
 			break;
 		case 0xe569f427: /* "QLinearConv" */
-			op = r->op_QLinearConv;
+			rop = r->op_QLinearConv;
 			break;
 		case 0xfe108481: /* "QLinearMatMul" */
-			op = r->op_QLinearMatMul;
+			rop = r->op_QLinearMatMul;
 			break;
 		case 0x37138211: /* "QuantizeLinear" */
-			op = r->op_QuantizeLinear;
+			rop = r->op_QuantizeLinear;
 			break;
 		case 0x0b881a13: /* "RNN" */
-			op = r->op_RNN;
+			rop = r->op_RNN;
 			break;
 		case 0xc100684f: /* "RandomNormal" */
-			op = r->op_RandomNormal;
+			rop = r->op_RandomNormal;
 			break;
 		case 0xa0b57174: /* "RandomNormalLike" */
-			op = r->op_RandomNormalLike;
+			rop = r->op_RandomNormalLike;
 			break;
 		case 0xf8e97c66: /* "RandomUniform" */
-			op = r->op_RandomUniform;
+			rop = r->op_RandomUniform;
 			break;
 		case 0x10a8b90b: /* "RandomUniformLike" */
-			op = r->op_RandomUniformLike;
+			rop = r->op_RandomUniformLike;
 			break;
 		case 0x73d06f69: /* "Reciprocal" */
-			op = r->op_Reciprocal;
+			rop = r->op_Reciprocal;
 			break;
 		case 0x7944853a: /* "ReduceL1" */
-			op = r->op_ReduceL1;
+			rop = r->op_ReduceL1;
 			break;
 		case 0x7944853b: /* "ReduceL2" */
-			op = r->op_ReduceL2;
+			rop = r->op_ReduceL2;
 			break;
 		case 0xeab46d14: /* "ReduceLogSum" */
-			op = r->op_ReduceLogSum;
+			rop = r->op_ReduceLogSum;
 			break;
 		case 0x9a057a01: /* "ReduceLogSumExp" */
-			op = r->op_ReduceLogSumExp;
+			rop = r->op_ReduceLogSumExp;
 			break;
 		case 0xa1d53763: /* "ReduceMax" */
-			op = r->op_ReduceMax;
+			rop = r->op_ReduceMax;
 			break;
 		case 0xdc7c323e: /* "ReduceMean" */
-			op = r->op_ReduceMean;
+			rop = r->op_ReduceMean;
 			break;
 		case 0xa1d53861: /* "ReduceMin" */
-			op = r->op_ReduceMin;
+			rop = r->op_ReduceMin;
 			break;
 		case 0xdc7e1072: /* "ReduceProd" */
-			op = r->op_ReduceProd;
+			rop = r->op_ReduceProd;
 			break;
 		case 0xa1d55372: /* "ReduceSum" */
-			op = r->op_ReduceSum;
+			rop = r->op_ReduceSum;
 			break;
 		case 0x20917223: /* "ReduceSumSquare" */
-			op = r->op_ReduceSumSquare;
+			rop = r->op_ReduceSumSquare;
 			break;
 		case 0x7c8bc29d: /* "Relu" */
-			op = r->op_Relu;
+			rop = r->op_Relu;
 			break;
 		case 0x9fdbcf8d: /* "Reshape" */
-			op = r->op_Reshape;
+			rop = r->op_Reshape;
 			break;
 		case 0xce8a9197: /* "Resize" */
-			op = r->op_Resize;
+			rop = r->op_Resize;
 			break;
 		case 0x5d77301a: /* "ReverseSequence" */
-			op = r->op_ReverseSequence;
+			rop = r->op_ReverseSequence;
 			break;
 		case 0x830cb9da: /* "RoiAlign" */
-			op = r->op_RoiAlign;
+			rop = r->op_RoiAlign;
 			break;
 		case 0x0e09b7cd: /* "Round" */
-			op = r->op_Round;
+			rop = r->op_Round;
 			break;
 		case 0x7c8c450a: /* "Scan" */
-			op = r->op_Scan;
+			rop = r->op_Scan;
 			break;
 		case 0xb4db6f18: /* "ScatterElements" */
-			op = r->op_ScatterElements;
+			rop = r->op_ScatterElements;
 			break;
 		case 0x55be5b0d: /* "ScatterND" */
-			op = r->op_ScatterND;
+			rop = r->op_ScatterND;
 			break;
 		case 0x7c8c4efe: /* "Selu" */
-			op = r->op_Selu;
+			rop = r->op_Selu;
 			break;
 		case 0xe537ccd3: /* "SequenceAt" */
-			op = r->op_SequenceAt;
+			rop = r->op_SequenceAt;
 			break;
 		case 0xa52772e3: /* "SequenceConstruct" */
-			op = r->op_SequenceConstruct;
+			rop = r->op_SequenceConstruct;
 			break;
 		case 0x5e6e772d: /* "SequenceEmpty" */
-			op = r->op_SequenceEmpty;
+			rop = r->op_SequenceEmpty;
 			break;
 		case 0x5e70f50e: /* "SequenceErase" */
-			op = r->op_SequenceErase;
+			rop = r->op_SequenceErase;
 			break;
 		case 0x35a57cb3: /* "SequenceInsert" */
-			op = r->op_SequenceInsert;
+			rop = r->op_SequenceInsert;
 			break;
 		case 0x3bff64e0: /* "SequenceLength" */
-			op = r->op_SequenceLength;
+			rop = r->op_SequenceLength;
 			break;
 		case 0x0e17a4d6: /* "Shape" */
-			op = r->op_Shape;
+			rop = r->op_Shape;
 			break;
 		case 0xd11575d4: /* "Shrink" */
-			op = r->op_Shrink;
+			rop = r->op_Shrink;
 			break;
 		case 0xf5548151: /* "Sigmoid" */
-			op = r->op_Sigmoid;
+			rop = r->op_Sigmoid;
 			break;
 		case 0x7c8c5f56: /* "Sign" */
-			op = r->op_Sign;
+			rop = r->op_Sign;
 			break;
 		case 0x0b8821ef: /* "Sin" */
-			op = r->op_Sin;
+			rop = r->op_Sin;
 			break;
 		case 0x7c8c6037: /* "Sinh" */
-			op = r->op_Sinh;
+			rop = r->op_Sinh;
 			break;
 		case 0x7c8c61c0: /* "Size" */
-			op = r->op_Size;
+			rop = r->op_Size;
 			break;
 		case 0x0e19f6b5: /* "Slice" */
-			op = r->op_Slice;
+			rop = r->op_Slice;
 			break;
 		case 0x6bec36a5: /* "Softplus" */
-			op = r->op_Softplus;
+			rop = r->op_Softplus;
 			break;
 		case 0x6bedcd32: /* "Softsign" */
-			op = r->op_Softsign;
+			rop = r->op_Softsign;
 			break;
 		case 0xa4436289: /* "SpaceToDepth" */
-			op = r->op_SpaceToDepth;
+			rop = r->op_SpaceToDepth;
 			break;
 		case 0x0e1c35d1: /* "Split" */
-			op = r->op_Split;
+			rop = r->op_Split;
 			break;
 		case 0x50e66fcd: /* "SplitToSequence" */
-			op = r->op_SplitToSequence;
+			rop = r->op_SplitToSequence;
 			break;
 		case 0x7c8c82cf: /* "Sqrt" */
-			op = r->op_Sqrt;
+			rop = r->op_Sqrt;
 			break;
 		case 0x08f69207: /* "Squeeze" */
-			op = r->op_Squeeze;
+			rop = r->op_Squeeze;
 			break;
 		case 0xf404645f: /* "StringNormalizer" */
-			op = r->op_StringNormalizer;
+			rop = r->op_StringNormalizer;
 			break;
 		case 0x0b88236f: /* "Sub" */
-			op = r->op_Sub;
+			rop = r->op_Sub;
 			break;
 		case 0x0b88237a: /* "Sum" */
-			op = r->op_Sum;
+			rop = r->op_Sum;
 			break;
 		case 0x0b882528: /* "Tan" */
-			op = r->op_Tan;
+			rop = r->op_Tan;
 			break;
 		case 0x7c8cca90: /* "Tanh" */
-			op = r->op_Tanh;
+			rop = r->op_Tanh;
 			break;
 		case 0x46fbf3df: /* "TfIdfVectorizer" */
-			op = r->op_TfIdfVectorizer;
+			rop = r->op_TfIdfVectorizer;
 			break;
 		case 0xa646ea33: /* "ThresholdedRelu" */
-			op = r->op_ThresholdedRelu;
+			rop = r->op_ThresholdedRelu;
 			break;
 		case 0x7c8cec53: /* "Tile" */
-			op = r->op_Tile;
+			rop = r->op_Tile;
 			break;
 		case 0x7c8d0643: /* "TopK" */
-			op = r->op_TopK;
+			rop = r->op_TopK;
 			break;
 		case 0x940b3944: /* "Transpose" */
-			op = r->op_Transpose;
+			rop = r->op_Transpose;
 			break;
 		case 0xd6278d9c: /* "Unique" */
-			op = r->op_Unique;
+			rop = r->op_Unique;
 			break;
 		case 0xc836156a: /* "Unsqueeze" */
-			op = r->op_Unsqueeze;
+			rop = r->op_Unsqueeze;
 			break;
 		case 0x0e601820: /* "Where" */
-			op = r->op_Where;
+			rop = r->op_Where;
 			break;
 		case 0x0b8837fe: /* "Xor" */
-			op = r->op_Xor;
+			rop = r->op_Xor;
 			break;
 
 		case 0x7c8388ee: /* "Celu" */
-			op = r->op_Celu;
+			rop = r->op_Celu;
 			break;
 		case 0x718dbc56: /* "DynamicQuantizeLinear" */
-			op = r->op_DynamicQuantizeLinear;
+			rop = r->op_DynamicQuantizeLinear;
 			break;
 		case 0x7b2541c8: /* "GreaterOrEqual" */
-			op = r->op_GreaterOrEqual;
+			rop = r->op_GreaterOrEqual;
 			break;
 		case 0x60d9a535: /* "LessOrEqual" */
-			op = r->op_LessOrEqual;
+			rop = r->op_LessOrEqual;
 			break;
 		case 0xf8c82769: /* "LogSoftmax" */
-			op = r->op_LogSoftmax;
+			rop = r->op_LogSoftmax;
 			break;
 		case 0xbb8f2396: /* "MeanVarianceNormalization" */
-			op = r->op_MeanVarianceNormalization;
+			rop = r->op_MeanVarianceNormalization;
 			break;
 		case 0x6ed111df: /* "NegativeLogLikelihoodLoss" */
-			op = r->op_NegativeLogLikelihoodLoss;
+			rop = r->op_NegativeLogLikelihoodLoss;
 			break;
 		case 0x0e01ebd2: /* "Range" */
-			op = r->op_Range;
+			rop = r->op_Range;
 			break;
 		case 0x034529c7: /* "Softmax" */
-			op = r->op_Softmax;
+			rop = r->op_Softmax;
 			break;
 		case 0x522154a3: /* "SoftmaxCrossEntropyLoss" */
-			op = r->op_SoftmaxCrossEntropyLoss;
+			rop = r->op_SoftmaxCrossEntropyLoss;
 			break;
 
 		default:
-			op = NULL;
+			rop = NULL;
 			break;
 		}
-		if(op)
-			op(n);
+		if(rop)
+			rop(n);
 	}
 }
 
@@ -1152,6 +1152,8 @@ struct onnx_context_t * onnx_context_alloc(const void * buf, size_t len, struct 
 			n->op = op_dummy;
 		if(n->init)
 			n->init(n);
+		if(n->op)
+			n->op(n);
 	}
 
 	return ctx;
