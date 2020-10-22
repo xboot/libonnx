5f9d790af11d2c11f19e4007e1517131c7f7d1cb
JianjunJiang <8192542@qq.com>
2020-10-22 20:56:40 +0800

    [onnx]change struct onnx_tensor_t

--- src/onnx.h
+++ src/onnx.h
@@ -64,8 +64,10 @@ union onnx_scalar_t {
 struct onnx_tensor_t {
 	char * name;
 	enum onnx_tensor_type_t type;
-	int64_t * dims, * datas;
-	int ndim, ndata;
+	int64_t * dims;
+	int ndim;
+	uint64_t * datas;
+	int ndata;
 	union onnx_scalar_t scalar;
 };
 
