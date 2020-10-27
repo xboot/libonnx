782b00de233ad918ed52efca6a9c1f4c0c4eea02
JianjunJiang <8192542@qq.com>
2020-10-27 20:29:19 +0800

    [operator]add Or operator

--- src/default/Or.c
+++ src/default/Or.c
@@ -1,5 +1,54 @@
 #include <onnx.h>
 
+static int Or_init(struct onnx_node_t * n)
+{
+	if((n->ninput == 2) && (n->noutput == 1))
+	{
+		struct onnx_tensor_t * a = n->inputs[0];
+		struct onnx_tensor_t * b = n->inputs[1];
+		struct onnx_tensor_t * y = n->outputs[0];
+		if(onnx_tensor_multi_broadcast_reshape(a, b, y, a->type))
+			return 1;
+	}
+	return 0;
+}
+
+static int Or_exit(struct onnx_node_t * n)
+{
+	return 1;
+}
+
+static void Or_bool(struct onnx_node_t * n)
+{
+	struct onnx_tensor_t * y = n->outputs[0];
+	struct onnx_tensor_t * a = n->inputs[0];
+	struct onnx_tensor_t * b = n->inputs[1];
+	uint8_t * py = (uint8_t *)y->datas;
+	uint8_t * pa;
+	uint8_t * pb;
+	int i, l;
+
+	if(onnx_tensor_multi_broadcast_reshape(a, b, y, a->type))
+	{
+		for(i = 0, l = y->ndata; i < l; i++)
+		{
+			pa = onnx_tensor_broadcast_map_address(a, y, i);
+			pb = onnx_tensor_broadcast_map_address(b, y, i);
+			py[i] = (*pa || *pb) ? 1 : 0;
+		}
+	}
+}
+
 void resolver_default_op_Or(struct onnx_node_t * n)
 {
+	switch(n->inputs[0]->type)
+	{
+	case ONNX_TENSOR_TYPE_BOOL:
+		n->init = Or_init;
+		n->exit = Or_exit;
+		n->op = Or_bool;
+		break;
+	default:
+		break;
+	}
 }
