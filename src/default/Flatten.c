242f0ad58768fe8a26535248c172ad0936d5606b
JianjunJiang <8192542@qq.com>
2020-10-30 14:20:22 +0800

    [operator]add Flatten operator

--- src/default/Flatten.c
+++ src/default/Flatten.c
@@ -1,5 +1,115 @@
 #include <onnx.h>
 
+struct operator_pdata_t {
+	int axis;
+};
+
+static int Flatten_init(struct onnx_node_t * n)
+{
+	struct operator_pdata_t * pdat;
+
+	if((n->ninput == 1) && (n->noutput == 1))
+	{
+		pdat = malloc(sizeof(struct operator_pdata_t));
+		if(pdat)
+		{
+			pdat->axis = onnx_attribute_read_int(n, "axis", 1);
+			n->priv = pdat;
+			return 1;
+		}
+	}
+	return 0;
+}
+
+static int Flatten_exit(struct onnx_node_t * n)
+{
+	struct operator_pdata_t * pdat = (struct operator_pdata_t *)n->priv;
+
+	if(pdat)
+		free(pdat);
+	return 1;
+}
+
+static int Flatten_reshape(struct onnx_node_t * n)
+{
+	struct operator_pdata_t * pdat = (struct operator_pdata_t *)n->priv;
+	struct onnx_tensor_t * x = n->inputs[0];
+	struct onnx_tensor_t * y = n->outputs[0];
+	int axis = pdat->axis;
+	int dims[x->ndim];
+	int ndim;
+	int i, j;
+
+	if(axis < 0)
+		axis += x->ndim;
+	if(axis < 0 || axis >= x->ndim)
+		return 0;
+	for(i = 0, j = 1, ndim = 0; i < x->ndim; i++)
+	{
+		if(i != axis)
+			j *= x->dims[i];
+		else
+		{
+			dims[ndim++] = j;
+			j = x->dims[i];
+		}
+	}
+	dims[ndim++] = j;
+	return onnx_tensor_reshape(y, dims, ndim, x->type);
+}
+
+static void Flatten_operator(struct onnx_node_t * n)
+{
+	struct onnx_tensor_t * x = n->inputs[0];
+	struct onnx_tensor_t * y = n->outputs[0];
+	char ** px = (char **)x->datas;
+	char ** py = (char **)y->datas;
+	int i, l;
+
+	if(x->type == ONNX_TENSOR_TYPE_STRING)
+	{
+		for(i = 0, l = y->ndata; i < l; i++)
+		{
+			if(py[i])
+				free(py[i]);
+			py[i] = strdup(px[i]);
+		}
+	}
+	else
+	{
+		if(x->ndim > 0)
+			memcpy(y->datas, x->datas, x->ndata * onnx_tensor_type_sizeof(x->type));
+		else
+			memcpy(&y->scalar, &x->scalar, sizeof(union onnx_scalar_t));
+	}
+}
+
 void resolver_default_op_Flatten(struct onnx_node_t * n)
 {
+	switch(n->inputs[0]->type)
+	{
+	case ONNX_TENSOR_TYPE_BOOL:
+	case ONNX_TENSOR_TYPE_INT8:
+	case ONNX_TENSOR_TYPE_INT16:
+	case ONNX_TENSOR_TYPE_INT32:
+	case ONNX_TENSOR_TYPE_INT64:
+	case ONNX_TENSOR_TYPE_UINT8:
+	case ONNX_TENSOR_TYPE_UINT16:
+	case ONNX_TENSOR_TYPE_UINT32:
+	case ONNX_TENSOR_TYPE_UINT64:
+	case ONNX_TENSOR_TYPE_BFLOAT16:
+	case ONNX_TENSOR_TYPE_FLOAT16:
+	case ONNX_TENSOR_TYPE_FLOAT32:
+	case ONNX_TENSOR_TYPE_FLOAT64:
+	case ONNX_TENSOR_TYPE_COMPLEX64:
+	case ONNX_TENSOR_TYPE_COMPLEX128:
+	case ONNX_TENSOR_TYPE_STRING:
+		n->init = Flatten_init;
+		n->exit = Flatten_exit;
+		n->reshape = Flatten_reshape;
+		n->operator = Flatten_operator;
+		break;
+	default:
+		break;
+	}
 }
